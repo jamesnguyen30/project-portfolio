6cf1edcfdce73df27b5bd75c51a13c2d57bb2632
Khuong Nguyen <nguyenthecoder@gmail.com>
2022-02-08 14:55:06 -0500

    Removed firebase project

--- old_api/functions/secrets/config.js
+++
@@ -1,9 +0,0 @@
-exports.firebaseConfig = {
-  apiKey: "AIzaSyAFJyVLp969fhcl1PGJp5NLibJVo5Ig224",
-  authDomain: "booksocialnetwork-4e3b7.firebaseapp.com",
-  projectId: "booksocialnetwork-4e3b7",
-  storageBucket: "booksocialnetwork-4e3b7.appspot.com",
-  messagingSenderId: "1016506394487",
-  appId: "1:1016506394487:web:28c21ea794f3e9eeb7a372",
-  measurementId: "G-6NESV8WVNG",
-};
--- old_api/functions/utils/firebase.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-module.exports = {admin, app};
