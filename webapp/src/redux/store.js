e7b652793262df938c458c87b88bfb3334fdc1ce
Khuong Nguyen <nguyenthecoder@gmail.com>
2022-02-10 18:12:20 -0500

    Playing around with eslint

---
+++ webapp/src/redux/store.js
@@ -0,0 +1,31 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initState = 0;
+
+const counterReducer = (type, state = initState) => {
+  switch (type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  counterReducer,
+});
+
+const composeEnhancer = composeWithDevTools({
+  name: 'BookSocialApp',
+});
+
+const enhancers = composeEnhancer(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancers);
+
+export default store;
