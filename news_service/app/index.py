238fc85ca6b38277500a463a0338064680df6c00
James Nguyen <nguyenthecoder@gmail.com.com>
2022-06-09 15:54:12 -0400

    add dotenv to news service app

--- news_service/app/index.py
+++ news_service/app/index.py
@@ -5,15 +5,15 @@ from db import news_db
 from routers import news
 import sys
 import pathlib
-
+import uvicorn
+import os
+from dotenv import load_dotenv
 
 cwd = pathlib.Path(__file__).parent
 sys.path.append(str(cwd))
-
-class Student(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = 0
-    year: Optional[int] = 0
+path = os.path.join(cwd, 'secrets', '.env')
+load_dotenv(dotenv_path = path)
+PORT = int(os.getenv("PORT"))
 
 app = FastAPI()
 
@@ -25,10 +25,7 @@ app.include_router(news.router)
 def health_check():
     return {'message': 'healthy'}
 
-# @app.get("/test_query")
-# def test_query(q: str = Query(default = 'anything', min_length=50, max_length=100, regex = '^whatever')):
-#     #process
-# NOTE: when set  Query(default = ...) --> means that the query value is required
-
+if __name__ == '__main__':
+    uvicorn.run("index:app", host = 'localhost', port = PORT, reload = True)
 
 
